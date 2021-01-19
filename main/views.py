0b7795fe6015519199f8fcc073ef6c20a72bd296
begaiym2015 <61770977+begaiym2015@users.noreply.github.com>
2021-01-19 18:44:58 +0300

    31st homework commit

--- main/views.py
+++ main/views.py
@@ -9,5 +9,16 @@ def test(request):
 def second(request):
     return HttpResponse("test 2 page")
 
+
+def homework(request):
+    return render(request, "homework31.html")
+
+def homework2(request):
+    return render(request, "homework312.html")
+
+def homework3(request):
+    return render(request, "homework3.html")
+
+
 # def third(request):
 #     return HttpResponse("This is page test3.")
--- todo/urls.py
+++ todo/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from main.views import homepage, test, second
+from main.views import homepage, test, second, homework, homework2, homework3
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -24,7 +24,10 @@ urlpatterns = [
     path("", homepage, name="home"),
     path("test", test, name="test"),
     path("test2/", second),
+    path("homework", homework, name="homework"),
+    path("homework2", homework2, name="homework2"),
+    path("homework3", homework3, name="homework3"),    
     # path("test3/", third),
 ]   + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    
+
